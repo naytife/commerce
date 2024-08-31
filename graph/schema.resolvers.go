ac8b7091a0e1181aa0514367d03077760502f138
petrejonn <petrejonn@gmail.com>
2024-08-31 15:46:52 +0100

    Update qgl schemat

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -9,8 +9,13 @@ import (
 	"fmt"
 
 	"github.com/go-faker/faker/v4"
+	"github.com/go-faker/faker/v4/pkg/options"
 	"github.com/petrejonn/ashia/graph/model"
 )
+
+func strPtr(s string) *string {
+	return &s
+}
 
 // CreateShop is the resolver for the createShop field.
 func (r *mutationResolver) CreateShop(ctx context.Context, shop model.CreateShopInput) (*model.Shop, error) {
@@ -21,15 +26,81 @@ func (r *mutationResolver) CreateShop(ctx context.Context, shop model.CreateShop
 func (r *queryResolver) Shop(ctx context.Context, id string) (*model.Shop, error) {
 	shop := &model.Shop{
 		ID:     id,
-		Title:  faker.Word(),
+		Title:  faker.Word(options.WithRandomStringLength(10)),
 		Domain: faker.DomainName(),
+		ContactPhone: &model.PhoneNumber{
+			Number:      faker.Phonenumber(),
+			CountryCode: "+234",
+		},
+		ContactEmail: strPtr(faker.Email()),
+		Location: &model.Location{
+			Address: faker.GetRealAddress().Address,
+			State:   faker.GetRealAddress().State,
+			Country: faker.GetRealAddress().State,
+		},
+		// Products     *ProductConnection
+		WhatsApp: &model.WhatsApp{
+			URL: faker.URL(),
+			Number: &model.PhoneNumber{
+				Number:      faker.Phonenumber(),
+				CountryCode: "+234",
+			},
+		},
+		Facebook: &model.Facebook{
+			URL:    faker.URL(),
+			Handle: faker.Word(),
+		},
+		SiteLogoURL: strPtr(faker.URL()),
+		FaviconURL:  strPtr(faker.URL()),
+		Currency:    strPtr(faker.Currency()),
+		Status:      (*model.ShopStatus)(strPtr(model.ShopStatusPublished.String())),
+		About:       strPtr(faker.Paragraph()),
+		UpdatedAt:   strPtr(faker.Timestamp()),
+		CreatedAt:   strPtr(faker.Timestamp()),
 	}
 	return shop, nil
 }
 
 // MyShops is the resolver for the myShops field.
 func (r *queryResolver) MyShops(ctx context.Context) ([]*model.Shop, error) {
-	panic(fmt.Errorf("not implemented: MyShops - myShops"))
+	shops := make([]*model.Shop, 5) // Generate 5 fake users
+	for i := range shops {
+		shops[i] = &model.Shop{
+			ID:     faker.UUIDHyphenated(),
+			Title:  faker.LastName(),
+			Domain: faker.DomainName(),
+			ContactPhone: &model.PhoneNumber{
+				Number:      faker.Phonenumber(),
+				CountryCode: "+234",
+			},
+			ContactEmail: strPtr(faker.Email()),
+			Location: &model.Location{
+				Address: faker.GetRealAddress().Address,
+				State:   faker.GetRealAddress().State,
+				Country: faker.GetRealAddress().State,
+			},
+			// Products     *ProductConnection
+			WhatsApp: &model.WhatsApp{
+				URL: faker.URL(),
+				Number: &model.PhoneNumber{
+					Number:      faker.Phonenumber(),
+					CountryCode: "+234",
+				},
+			},
+			Facebook: &model.Facebook{
+				URL:    faker.URL(),
+				Handle: faker.Word(),
+			},
+			SiteLogoURL: strPtr(faker.URL()),
+			FaviconURL:  strPtr(faker.URL()),
+			Currency:    strPtr(faker.Currency()),
+			Status:      (*model.ShopStatus)(strPtr(model.ShopStatusPublished.String())),
+			About:       strPtr(faker.Paragraph()),
+			UpdatedAt:   strPtr(faker.Timestamp()),
+			CreatedAt:   strPtr(faker.Timestamp()),
+		}
+	}
+	return shops, nil
 }
 
 // Mutation returns MutationResolver implementation.
