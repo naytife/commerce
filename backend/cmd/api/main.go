f0a50ce08845914303b92a64b5def9e095028058
petrejonn <petrejonn@gmail.com>
2024-09-15 19:28:37 +0100

    Use os model to get env variable

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -29,10 +29,13 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/", graph.NewPlaygroundHandler("/query"))
 	mux.Handle("/query", auth.JWTMiddleware()(graph.NewHandler(repo)))
-	log.Println(env.PORT)
-	log.Println(env.AUTH0_DOMAIN)
-	log.Println(env.DATABASE_URL)
-	log.Println(env.AUTH0_AUDIENCE)
+	// log.Println(env.PORT)
+	// log.Println(env.AUTH0_DOMAIN)
+	// log.Println(env.DATABASE_URL)
+	// log.Println(env.AUTH0_AUDIENCE)
+	log.Println(os.Getenv("PORT"))
+	log.Println(os.Getenv("AUTH0_DOMAIN"))
+	log.Println(os.Getenv("DATABASE_URL"))
 
 	// run the server
 	port := "0.0.0.0:8080" // + env.PORT
