e5bf49ae84fd94bc27b3da2e05eeba8ebe4924d2
petrejonn <petrejonn@gmail.com>
2024-09-23 20:47:20 +0100

    Restructure folders/files

--- dashboard/src/routes/(protected)/p/create/+page.server.ts
+++ dashboard/src/routes/(protected)/p/create/+page.server.ts
@@ -1,6 +1,6 @@
 import type { PageServerLoad } from './$types.js';
 import { superValidate } from 'sveltekit-superforms';
-import { formSchema } from './schema';
+import { formSchema } from './schema.js';
 import { zod } from 'sveltekit-superforms/adapters';
 
 export const load: PageServerLoad = async () => {
--- dashboard/src/routes/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Welcome to hellocart</h1>
