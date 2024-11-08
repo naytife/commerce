2c9d5bb6a81720b95c603d4bbee3f9c26cad4189
Emereh-boop <abuashiaemereh@gmail.com>
2024-11-08 09:31:45 +0100

    added the first user shop template

--- template_1/src/lib/components/ui/tabs/tabs-list.svelte
+++ template_1/src/lib/components/ui/tabs/tabs-list.svelte
@@ -10,7 +10,7 @@
 
 <TabsPrimitive.List
 	class={cn(
-		'bg-muted text-muted-foreground inline-flex h-9 items-center justify-center rounded-lg p-1',
+		'bg-muted text-muted-foreground inline-flex h-10 items-center justify-center rounded-md p-1',
 		className
 	)}
 	{...$$restProps}
