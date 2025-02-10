60a7a736a6c3f36b4700efd195104d573c45fa87
Erimebe <petrejonn@gmail.com>
2025-02-10 14:16:08 +0100

    add swagger

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/swagger"
 	"github.com/petrejonn/naytife/config"
 	"github.com/petrejonn/naytife/internal/api/models"
 	"github.com/petrejonn/naytife/internal/api/routes"
@@ -17,6 +18,17 @@ import (
 	"github.com/petrejonn/naytife/internal/middleware"
 )
 
+// @title Naytife API Docs
+// @version 1.0
+// @description This is the Naytife API documentation
+// @host 127.0.0.1:8080
+// @BasePath /api/v1
+// @schemes http
+// @securityDefinitions.oauth2.accessCode OAuth2AccessCode
+// @tokenUrl http://127.0.0.1:8080/oauth2/token
+// @authorizationUrl http://127.0.0.1:8080/oauth2/auth
+// @scope.openid "OpenID scope"
+// @scope.profile "Profile scope"
 func main() {
 	env, err := config.LoadConfig()
 	if err != nil {
@@ -47,9 +59,26 @@ func main() {
 		AllowHeaders: "Content-Type,Authorization",
 	}))
 	app.Use(logger.New())
+	app.Get("/api/v1/docs/swagger.json", func(c *fiber.Ctx) error {
+		return c.SendFile("./docs/swagger.json") // or your own handler logic
+	})
+	app.Get("/api/v1/docs/*", swagger.New(swagger.Config{ // custom
+		URL:         "http://127.0.0.1:8080/api/v1/docs/swagger.json",
+		DeepLinking: false,
+		// Expand ("list") or Collapse ("none") tag groups by default
+		DocExpansion: "none",
+		// Prefill OAuth ClientId on Authorize popup
+		OAuth: &swagger.OAuthConfig{
+			AppName:      "OAuth Provider",
+			ClientId:     "f748b964-7de3-4159-9419-ae6e78502dc1",
+			ClientSecret: "_6EHdptai8.SwLKodQxhpv3SKm",
+		},
+		// Ability to change OAuth2 redirect uri location
+		OAuth2RedirectUrl: "http://localhost:8080/api/v1/docs/oauth2-redirect.html",
+	}))
 
-	// api := app.Group("/api/v1")
-	api := app.Group("/api/v1", middleware.WebMiddlewareFiber())
+	v1 := app.Group("/api/v1/")
+	api := v1.Group("/*", middleware.WebMiddlewareFiber())
 	routes.ShopRouter(api, repo)
 	routes.UserRouter(api, repo)
 	routes.CartRouter(api, repo)
@@ -57,8 +86,8 @@ func main() {
 	app.Get("/api/graph", publicgraph.NewPlaygroundHandler("/api/query"))
 	app.Get("/api/admin/graph", admingraph.NewPlaygroundHandler("/api/admin/query"))
 
-	graphql := app.Group("/api", middleware.ShopIDMiddlewareFiber(repo))
-	graphql.Post("/query", publicgraph.NewHandler(repo))      // public
+	graphql := app.Group("/api/query", middleware.ShopIDMiddlewareFiber(repo))
+	graphql.Post("/", publicgraph.NewHandler(repo))           // public
 	graphql.Post("/admin/query", admingraph.NewHandler(repo)) // admin
 
 	address := ":" + env.PORT
